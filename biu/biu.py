ee7111d1f12a866e3cfd02448abbe583bba07bde
Yilun Guan <zoom.aaron@gmail.com>
2019-10-07 01:29:14 -0400

    Some usability improvements

--- biu/biu.py
+++ biu/biu.py
@@ -1,27 +1,41 @@
-#!/bin/env/python
+#!/bin/env python
 # -*- coding: utf-8 -*-
 import sys
 import os
 import importlib
 import glob
 
+# setup look-up directory properly
+CONF_DIR = os.path.join(os.environ['HOME'] + '/.biu.d/')
+sys.path.append(CONF_DIR)
+
 def main():
-    Home = os.environ['HOME']
-    conf_dir = Home + '/.biu.d/'
-    sys.path.append(conf_dir)
+    # show all available commands
     if len(sys.argv) == 1:
-        print("All commmands:")
-        for filename in glob.glob(conf_dir + '*.py'):
+        print("Available biu commands:")
+        for filename in glob.glob(CONF_DIR + '*.py'):
             filename = os.path.basename(filename).split('.')[0]
-            print(filename)
+            print(f"- {filename}")
     else:
-        biu = importlib.import_module(sys.argv[1])
+        mod_name = sys.argv[1]
+        mod = importlib.import_module(mod_name)
         if len(sys.argv) == 2:
-            getattr(biu,"help")()
+            try:
+                ret = getattr(mod, "help")()
+            except Exception:
+                print(f"No help found for {mod_name}!")
         elif len(sys.argv) == 3:
-            getattr(biu,sys.argv[2])()
+            ret = getattr(mod, sys.argv[2])()
         else:
-            getattr(biu,sys.argv[2])(sys.argv[3:])
+            ret = getattr(mod, sys.argv[2])(*sys.argv[3:])
+
+        if isinstance(ret, list):
+            print("======================")
+            print("Run: \n")
+            for cmd in ret:
+                print(cmd)
+                os.system(cmd)
+            print("======================")
     sys.exit()
 
 if __name__ == '__main__':
