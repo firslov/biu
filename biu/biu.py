5f8a76633aff9161ded2911263c5bf8798cf3dbc
Yilun Guan <zoom.aaron@gmail.com>
2019-10-09 18:13:26 -0400

    Add a special command to list all recipes

--- biu/biu.py
+++ biu/biu.py
@@ -9,27 +9,52 @@ import glob
 CONF_DIR = os.path.join(os.environ['HOME'] + '/.biu.d/')
 sys.path.append(CONF_DIR)
 
+def list_of_commands():
+    """Return available biu commands"""
+    files = [f for f in glob.glob(CONF_DIR+'*.py')]
+    return [os.path.basename(f).split('.')[0] for f in files]
+
+def list_of_recipes(mod):
+    """Return available recipes in a module"""
+    d = mod.__dict__
+    return [f for f in d if "<f" in str(d[f])]
+
+def list_of_all_recipes():
+    "Return all available recipes across all modules"
+    mod_names = list_of_commands()
+    recipes = []
+    for mod_name in mod_names:
+        mod = importlib.import_module(mod_name)
+        rs = list_of_recipes(mod)
+        recipes.extend([f"{mod_name} {r}" for r in rs])
+    return recipes
+
 def main():
+    ret = None
+
     # show all available commands
     if len(sys.argv) == 1:
         print("Available biu commands:")
-        for filename in glob.glob(CONF_DIR + '*.py'):
-            filename = os.path.basename(filename).split('.')[0]
+        for filename in list_of_commands():
             print(f"- {filename}")
     else:
         mod_name = sys.argv[1]
-        mod = importlib.import_module(mod_name)
         if len(sys.argv) == 2:
-            ## print all functions
-            print("Available commands:")
-            dic = mod.__dict__
-            for val in dic:
-                if "<function " in str(dic[val]):
-                    print(val)
-            ret = None
+            # check for special keywords
+            if mod_name == "list":
+                for r in list_of_all_recipes():
+                    print(r)
+            else:
+                # print all functions
+                mod = importlib.import_module(mod_name)
+                print(f"Available {mod_name} recipes:")
+                for r in list_of_recipes(mod):
+                    print(f"- {r}")
         elif len(sys.argv) == 3:
+            mod = importlib.import_module(mod_name)
             ret = getattr(mod, sys.argv[2])()
         else:
+            mod = importlib.import_module(mod_name)
             ret = getattr(mod, sys.argv[2])(*sys.argv[3:])
 
         if isinstance(ret, list):
