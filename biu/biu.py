e21887db9edf800270f05f942e507f4e075fce1e
firslov <345989060@qq.com>
2019-10-10 10:19:37 +0800

    fix error message

--- biu/biu.py
+++ biu/biu.py
@@ -30,40 +30,42 @@ def list_of_all_recipes():
     return recipes
 
 def main():
-    ret = None
-
-    # show all available commands
-    if len(sys.argv) == 1:
-        print("Available biu commands:")
-        for filename in list_of_commands():
-            print(f"- {filename}")
-    else:
-        mod_name = sys.argv[1]
-        if len(sys.argv) == 2:
-            # check for special keywords
-            if mod_name == "list":
-                for r in list_of_all_recipes():
-                    print(r)
-            else:
-                # print all functions
-                mod = importlib.import_module(mod_name)
-                print(f"Available {mod_name} recipes:")
-                for r in list_of_recipes(mod):
-                    print(f"- {r}")
-        elif len(sys.argv) == 3:
-            mod = importlib.import_module(mod_name)
-            ret = getattr(mod, sys.argv[2])()
+    try:
+        ret = None
+        # show all available commands
+        if len(sys.argv) == 1:
+            print("Available biu commands:")
+            for filename in list_of_commands():
+                print(f"- {filename}")
         else:
-            mod = importlib.import_module(mod_name)
-            ret = getattr(mod, sys.argv[2])(*sys.argv[3:])
+            mod_name = sys.argv[1]
+            if len(sys.argv) == 2:
+                # check for special keywords
+                if mod_name == "list":
+                    for r in list_of_all_recipes():
+                        print(r)
+                else:
+                    # print all functions
+                    mod = importlib.import_module(mod_name)
+                    print(f"Available {mod_name} recipes:")
+                    for r in list_of_recipes(mod):
+                        print(f"- {r}")
+            elif len(sys.argv) == 3:
+                mod = importlib.import_module(mod_name)
+                ret = getattr(mod, sys.argv[2])()
+            else:
+                mod = importlib.import_module(mod_name)
+                ret = getattr(mod, sys.argv[2])(*sys.argv[3:])
 
-        if isinstance(ret, list):
-            print("======================")
-            print("Run: \n")
-            for cmd in ret:
-                print(cmd)
-                os.system(cmd)
-            print("======================")
+            if isinstance(ret, list):
+                print("======================")
+                print("Run: \n")
+                for cmd in ret:
+                    print(cmd)
+                    os.system(cmd)
+                print("======================")
+    except:
+        print("Illegal command, please input again.")
     sys.exit()
 
 if __name__ == '__main__':
